686d62d0f9c3020018969d0813a8500f86c8413f
Julien <nttmxk@korea.ac.kr>
2022-09-13 23:38:43 +0900

    Refactor: ex00

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,17 +1,19 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
 # include <iostream>
 
 class Fixed {
 private:
 	int value;
-	const static int fract;
+	static const int fract = 8;
 
 public:
 	Fixed();
 	Fixed(const Fixed &orig);
 	Fixed& operator=(const Fixed &orig);
 	~Fixed();
+
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
 };
