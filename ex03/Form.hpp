cb1101a867750b1c7d7cded80bb45ce11fe60293
jinoh <nttmxk@korea.ac.kr>
2022-11-21 17:51:31 +0900

    Done: complete ex03

---
+++ ex03/Form.hpp
@@ -0,0 +1,43 @@
+#ifndef FORM_HPP
+
+# define FORM_HPP
+# include <iostream>
+# include <exception>
+
+class Bureaucrat;
+
+class Form {
+private:
+	std::string const name;
+	const int grade_sign;
+	const int grade_exec;
+	bool sign;
+	static const int highestGrade = 1;
+	static const int lowestGrade = 150;
+
+public:
+	class GradeTooHighException : public std::exception{
+	public:
+		const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception{
+	public:
+		const char *what() const throw();
+	};
+	Form();
+	Form(std::string const &name, int grade_sign, int grade_exec);
+	virtual ~Form();
+	Form(const Form &src);
+	Form& operator=(const Form &src);
+	void beSigned(Bureaucrat &src);
+	bool getSign() const;
+	const std::string &getName() const;
+	int getGradeSign() const;
+	int getGradeExec() const;
+	virtual int execute(Bureaucrat const &executor) const = 0;
+};
+
+std::ostream& operator<<(std::ostream& outputStream, const Form &src);
+
+
+#endif
---
+++ ex03/ShrubberyCreationForm.hpp
@@ -0,0 +1,26 @@
+#ifndef SHRUBBERYCREATIONFORM_HPP
+
+# define SHRUBBERYCREATIONFORM_HPP
+# include "Form.hpp"
+# include <iostream>
+# include <fstream>
+
+class Bureaucrat;
+
+class ShrubberyCreationForm: public Form {
+private:
+	std::string const name;
+	bool sign;
+	static const int grade_sign = 145;
+	static const int grade_exec = 137;
+
+public:
+	ShrubberyCreationForm();
+	ShrubberyCreationForm(std::string const &name);
+	~ShrubberyCreationForm();
+	ShrubberyCreationForm(const ShrubberyCreationForm &src);
+	ShrubberyCreationForm& operator=(const ShrubberyCreationForm &src);
+	int execute(Bureaucrat const &executor) const;
+};
+
+#endif
