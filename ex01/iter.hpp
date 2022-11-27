bdf490a9777fe6d22a80c79ecdd068dfb66bdf03
jinoh <nttmxk@korea.ac.kr>
2022-11-27 22:02:01 +0900

    Done: complete ex01

---
+++ ex01/iter.hpp
@@ -0,0 +1,19 @@
+#ifndef ITER_HPP
+
+# define ITER_HPP
+# include <iostream>
+
+template <typename T>
+void	iter(T* arr, unsigned int len, void (*func)(T&))
+{
+	for (unsigned int i = 0; i < len; ++i)
+		func(arr[i]);
+}
+
+template <typename T>
+void printArr(T &a)
+{
+	std::cout << a << std::endl;
+}
+
+#endif
