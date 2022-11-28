eb7d8455d4fc447ba4c4e7d12ca66413b8e2a0ab
jinoh <nttmxk@korea.ac.kr>
2022-11-28 18:03:05 +0900

    Fix: now iter function can handle function pointer which takes const argument

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -4,7 +4,7 @@
 # include <iostream>
 
 template <typename T>
-void	iter(T* arr, unsigned int len, void (*func)(T&))
+void	iter(T* arr, unsigned int len, void (*func)(T const &))
 {
 	for (unsigned int i = 0; i < len; ++i)
 		func(arr[i]);
