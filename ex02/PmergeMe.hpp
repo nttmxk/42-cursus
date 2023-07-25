1fdac433afbb2a78d54e7181d5f067b4cc279725
jinoh <nttmxk@korea.ac.kr>
2023-07-25 17:44:59 +0900

    Feat: Ford-Johnson sorting function

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -2,33 +2,37 @@
 # define PMERGEME_HPP
 
 # include <iostream>
+# include <algorithm>
 # include <sstream>
 # include <cstring>
 # include <vector>
-# include <list>
-# include <algorithm>
+# include <deque>
 # include <exception>
 # include <ctime>
+# include <utility>
+
+# include <random>
+# define ELEMENT_SIZE 5000
+# define TRIAL 1
 
 class PmergeMe
 {
 private:
 	std::vector<int>	input;
 	std::vector<int>	v;
-	std::list<int>		l;
+	std::deque<int>		d;
 
-	void	checkInput(const char *s);
-	void 	sortVector();
-	void	sortList();
+	void				checkInput(const char *s);
+	std::vector<int>	sortVector(std::vector<int>::iterator first, std::vector<int>::iterator last, int depth);
+	std::deque<int>		sortDeque(std::deque<int>::iterator first, std::deque<int>::iterator last, int depth);
+
 public:
 	PmergeMe();
 	~PmergeMe();
 	PmergeMe(const PmergeMe &src);
 	PmergeMe& operator=(const PmergeMe &src);
-
-	void	sort(int n, const char **argv);
+	void	sort(int n, char **argv);
+	void	sortTest();
 };
-
-
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,8 +1,23 @@
 #include "PmergeMe.hpp"
+#include <iostream>
+
+void check_leaks()
+{
+	system("leaks PmergeMe");
+}
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+//	check_leaks();
+
+//	PmergeMe test;
+//	try {
+//		test.sortTest();
+//	} catch (std::exception &e) {
+//		std::cout << e.what() << '\n';
+//	}
+
+	if (argc < 2 || argc > 10000000)
 	{
 		std::cout << "Error\n";
 		return (1);
