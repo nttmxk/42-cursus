a5897473bbc8c717a7f664e7683f7353435825c2
julien <nttmxk@korea.ac.kr>
2023-09-21 16:29:16 +0900

    feat: add all the files to organize docker compose

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ ! -f "/var/www/html/index.php" ]; then
+	wp core download
+	wp config create \
+		--dbname=${MYSQL_DATABASE} \
+		--dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_USER_PASSWORD \
+		--dbhost=mariadb:3306
+	wp core install \
+		--url=http://jinoh.42.fr \
+		--title="jinoh's blog" \
+		--admin_user=$WP_ADMIN \
+		--admin_password=$WP_ADMIN_PASSWORD \
+		--admin_email=$WP_ADMIN_EMAIL
+
+#if [ "$WP_USER" = "" ]; then
+	wp user create ${WP_USER} ${WP_USER_EMAIL} --role=author --user_pass=${WP_USER_PASSWORD}
+fi
+
+php-fpm -F
